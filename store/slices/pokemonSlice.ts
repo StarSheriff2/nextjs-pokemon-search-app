8a78c03852c33c64bb09336e5ff71925b564dc53
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-02-23 16:17:48 -0600

    Remove comments

--- store/slices/pokemonSlice.ts
+++ store/slices/pokemonSlice.ts
@@ -5,18 +5,6 @@ import IPokemon from '../types/IPokemon.type';
 const createPokemonSlice: StateCreator<IPokemon> = (set, get) => ({
   data: null,
   setData: (data: Pokemon) => set({ data }),
-  // isLoading: false,
-  // error: null,
-  // getPokemon: (limit: number) => {
-  //   set({ isLoading: true });
-  //   if (isError) {
-  //     set({ error: (error as any).message, isLoading: false });
-  //   } else {
-  //     set({ isLoading: false, data });
-  //   }
-  // },
 });
 
 export default createPokemonSlice;
-
-// useQuery(key, queryFn, { onSuccess: (data) => setToZustand(data) });
